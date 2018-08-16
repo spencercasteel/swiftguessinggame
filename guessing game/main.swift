6768958347bff66e4cff365c5f87ff9e68e6e8cc
Spencer Casteel <spencercasteel@interapts-macbook-pro-9.local>
2018-08-16 10:22:03 -0500

    Initial Commit

---
+++ guessing game/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  guessing game
+//
+//  Created by Spencer Casteel on 8/16/18.
+//  Copyright © 2018 Spencer Casteel. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
